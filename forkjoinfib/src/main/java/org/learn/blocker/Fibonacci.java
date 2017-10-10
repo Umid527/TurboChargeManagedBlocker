b39a3da2bdd821d5fdef6a0515318590c6a8a8a0
umid527 <umid.ma@gmail.com>
2017-10-10 17:11:35 +0400

    demo 1

--- forkjoinfib/src/main/java/org/learn/blocker/Fibonacci.java
+++ forkjoinfib/src/main/java/org/learn/blocker/Fibonacci.java
@@ -7,11 +7,23 @@ import java.util.concurrent.RecursiveTask;
  * @author umahmudov on 09-Oct-17.
  * @project ManagedBlocked
  */
+
+//      demo1: test100_000_000() time = 42332
 public class Fibonacci {
     public BigInteger f(int n) {
         if (n == 0) return BigInteger.ZERO;
         if (n == 1) return BigInteger.ONE;
 
-        return f(n - 1).add(f(n - 2));
+        int half=(n+1)/2;
+
+        BigInteger f0=f(half-1);
+        BigInteger f1= f(half);
+
+        if(n%2==1){
+            return f0.multiply(f0).add(f1.multiply(f1));
+        }else{
+            return f0.shiftLeft(1).add(f1).multiply(f1);
+        }
+//        return f(n - 1).add(f(n - 2));
     }
 }
