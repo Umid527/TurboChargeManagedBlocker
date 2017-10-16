e39ba12cba16b89200b245e5227977d71aaa9a06
umid527 <umid.ma@gmail.com>
2017-10-16 17:07:17 +0400

    demo 3 with timing

--- forkjoinfib/src/main/java/org/learn/blocker/Fibonacci.java
+++ forkjoinfib/src/main/java/org/learn/blocker/Fibonacci.java
@@ -29,10 +29,17 @@ public class Fibonacci {
         BigInteger f1 = f(half);
         BigInteger f0 = f0_task.join();
 
+        long time =n>10_000 ?System.currentTimeMillis():0;
+        try{
+
         if (n % 2 == 1) {
             return f0.multiply(f0).add(f1.multiply(f1));
         } else {
             return f0.shiftLeft(1).add(f1).multiply(f1);
+        }
+        }finally {
+            time =n>10_000 ?System.currentTimeMillis()-time:0;
+            if(time>50)System.out.printf("f(%d) took %d%n",n,time);
         }
 //        return f(n - 1).add(f(n - 2));
     }
